c2edda91fb8005115749555d6228e54553da7e9b
Cuppachino <92656523+Cuppachino@users.noreply.github.com>
2022-04-30 20:31:46 -0500

    refactor: clean up footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import LegalPolicy from './LegalPolicy';
 
+const Footer = () => {
+  return (
+    <footer className=' bg-neutral-100 dark:bg-neutral-900 absolute bottom-0 inset-x-0 h-52 sm:h-44 md:h-40 px-8 py-6 md:py-8'>
+      <ul className='breakpoints-default flex justify-between items-center w-full'>
+        <LegalPolicy />
+        <li className='grid gap-4  text-center'>
+          <span className='group select-none whitespace-nowrap text-sm bg-neutral-900 dark:bg-neutral-300 text-neutral-300 dark:text-black dark:font-semibold px-3 py-2 hover:-translate-y-1 transition-all duration-150'>
+            <a
+              href='https://github.com/MatteoLeonesi'
+              className='group-hover:text-white dark:group-hover:text-neutral-800 w-fit'>
+              Github Repository
+            </a>
+          </span>
+          <span className='select-text whitespace-nowrap text-neutral-700 dark:text-neutral-400'>
+            © 2022 Matteo Leonesi
+          </span>
+        </li>
+      </ul>
+    </footer>
+  );
+};
 
-function Footer() {
-    return (
-        <footer className="p-4 bg-white rounded-lg  md:flex md:items-center md:justify-between md:p-6 mt-10 mr-5 ml-10 dark:bg-gray-800" >
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://github.com/MatteoLeonesi" className="hover:underline">Github Repository</a>
-            </span>
-            <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-                <p>Is not an official Riot Wiki ! </p>
-            </ul>
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;

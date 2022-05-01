061762e6cb110a5959f05e497b7a9e9eff5bc160
Cuppachino <92656523+Cuppachino@users.noreply.github.com>
2022-04-30 20:49:41 -0500

    feat: create Weapons.tsx page
    should turn the tailwind classes on it into a reusable "page-content" class.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Home from './pages/Home';
+import Weapons from './pages/Weapons';
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className='App '>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='weapons' element={<Weapons />} />
         <Route path='about' element={<About />} />
       </Routes>
       <Footer />
